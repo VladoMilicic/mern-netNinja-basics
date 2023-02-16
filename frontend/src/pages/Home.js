e5b35b5fdc657d5b36182d59ad58ad26b4fc6324
VladoMilicic <vlado_milicic@hotmail.com>
2023-02-17 00:50:41 +0100

    frontend added

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, []) //pokrece funkciju cim se stranica ucita, da se ne bi vise puta ucitavala kao drugi arg se dodaje prazan niz
+
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails workout={workout} key={workout._id} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+export default Home;
